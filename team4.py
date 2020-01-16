77952dbeeb81d079943263309a9fc1bf0e581b94
Tran025 <59578231+Tran025@users.noreply.github.com>
2020-01-16 12:25:10 -0600

    Update team4.py

--- team4.py
+++ team4.py
@@ -1,3 +1,4 @@
+import random
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -6,18 +7,23 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'RNG Gang' # Only 10 chars displayed.
+strategy_name = 'The Dice Roll'
+strategy_description = 'Roll a random number between 1-10 if it is 5 or lower we collude but over 5 we betray'
     
-def move(my_history, their_history, my_score, their_score):
+def move(my_history, their_history,my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
 
+    Make my move.
+    Returns 'c' or 'b'.
+    '''
+    rng = random.randint(1,11)
+    if rng <= 5:
+        return 'c'
+    else:
+        return 'b'
+   
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -26,7 +32,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+   
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
