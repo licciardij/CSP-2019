9d93cd318c840ae9646408c7b2f573969390eae8
willi077 <59578370+willi077@users.noreply.github.com>
2020-01-17 11:05:12 -0600

    Update team5.py

--- team5.py
+++ team5.py
@@ -26,7 +26,16 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    if (my_history)==0:
+        return 'c'
+    else:
+        return 'b'
+    if len (my_history)==0: 
+        return 'b'
+    elif my_history[-1]=='c' and their_history[-1]=='b': 
+        return 'c' 
+    else: 
+        return 'c' 
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
