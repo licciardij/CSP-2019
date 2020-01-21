0779cf01600901dd4aa3cd75cf871b3c72bb56c1
marsh012 <59578263+marsh012@users.noreply.github.com>
2020-01-21 11:48:00 -0600

    Update team8.py

--- team8.py
+++ team8.py
@@ -26,7 +26,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    return 'b'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
@@ -65,4 +65,5 @@ if __name__ == '__main__':
               # move('bbb', 'ccc', 0, 0) returns 'b'.
               my_score=0, 
               their_score=0,
-              result='b')             
+              result='b')
+   # by team 8
