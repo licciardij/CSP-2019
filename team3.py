f58e6bc8a623cf8d844b1b0a65ca84d80267f220
aloren004 <59578363+aloren004@users.noreply.github.com>
2020-01-22 13:38:24 -0600

    Update team3.py

--- team3.py
+++ team3.py
@@ -7,26 +7,23 @@
 ####
 
 team_name = 'Team 3' # Only 10 chars displayed.
-strategy_name = 'Learn opponent strategy'
-strategy_description = 'try to figure out what strategy the opponent is using'
+strategy_name = 'mem_cCBBC'
+strategy_description = 'winner of test runs'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    if len(my_history) == 0:
+        return 'c'
+    else:
+        if my_history[len(my_history)-1] == 'c':
+            if their_history[len(their_history)-1] == 'c':
+                return 'c'
+            else:
+                return 'b'
+        elif my_history[len(my_history)-1] == 'b':
+            if their_history[len(their_history)-1] == 'c':
+                return 'b'
+            else:
+                return 'C'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
